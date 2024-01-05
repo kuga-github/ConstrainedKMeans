6c40bf03b07c92997478e20cb96e4af24a91ad96
Kuga Tsunekawa <79348631+kuga-github@users.noreply.github.com>
2024-01-05 21:50:06 +0900

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -61,7 +61,7 @@ class ConstrainedKMeans:
             rand_vals = self.random_state.uniform(size=n_local_trials) * current_pot
             # すでに中心点に選ばれたデータは最近傍中心点との二乗ユークリッドが0になるので、累積和を取ると同じ値が連続することになり、
             # searchsortedによってそれらのインデックスを避けて中心点候補をサンプリングできる　(a[i-1] < v <= a[i])
-            # 一様分布を変数変換し、重み付けした確率分布からサンプリングするのと同義
+            # 一様分布を変数変換し、最近傍中心点との二乗ユークリッドによって重み付けした確率分布からサンプリングするのと同義
             candidate_ids = np.searchsorted(np.cumsum(closest_dist_sq), rand_vals)
             # out of range防止
             np.clip(candidate_ids, None, closest_dist_sq.size - 1, out=candidate_ids)
